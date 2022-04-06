e4657ef331da75c6f89df2608bef5871fd903b1b
N4kka <sarlinicola7@gmail.com>
2022-04-06 16:12:32 +0200

    Script added: ask to the user First and Last name + his favourite colour and generate a password

--- js/text.js
+++ js/text.js
@@ -1,5 +1,22 @@
-//Asking the user some infos to generate a password
+//Asking the user some infos to generate a password:
 
 //Declaration
-// const UserName(prompt('name'));
-// console.log(UserName, typeof(UserName));
+//Name
+const userFirstName = prompt('First Name');
+console.log(userFirstName, typeof(userFirstName));
+
+//LastName
+const userLastName = prompt('Last Name');
+console.log(userLastName, typeof (userLastName));
+
+//Favourite Colour 
+const userColour = prompt('Colour');
+console.log(userColour, typeof (userColour));
+
+//Password Generator
+const result = userFirstName + userLastName + userColour + 21;
+console.log(result);
+
+//OUTPUT
+//Showing the password generated
+document.getElementById('my-title').innerHTML = result;
